a752358c2fea2fb699414114c22823f295d25607
Rostislav <khmelinin.r@gmail.com>
2022-04-28 21:09:16 +0900

    basic php lesson 1 task 2

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ $userName = readline("Как зовут тебя пользователь?\n ");
 
 $userYearBirth = readline("OK, в каком году ты родился, от Рождества Христова?\n ");
 
-$userAge = date("Y")-$userYearBirth;
+$userAge = date("Y") - $userYearBirth;
 
 echo "Приветствую тебя, $userAge летний пользователь $userName !!!\n";
