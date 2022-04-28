96032da7dee5e0d532a6d350fd02d300401512be
Rostislav <khmelinin.r@gmail.com>
2022-04-28 21:01:21 +0900

    basis php lesson 1 task 1

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo "hello";
+
+$userName = readline("Как зовут тебя пользователь?\n ");
+
+$userYearBirth = readline("OK, в каком году ты родился, от Рождества Христова?\n ");
+
+$userAge = date("Y")-$userYearBirth;
+
+echo "Приветствую тебя, $userAge летний пользователь $userName !!!\n";
