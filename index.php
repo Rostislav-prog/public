0f990ce9759f62882c1db43d4b8aa10d7081375c
Rostislav <khmelinin.r@gmail.com>
2022-04-28 22:04:22 +0900

    basik php lesson 1 task 3

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 
-$userName = readline("Как зовут тебя пользователь?\n ");
+$userName = readline("Как зовут вас пользователь?\n ");
 
-$userYearBirth = readline("OK, в каком году ты родился, от Рождества Христова?\n ");
+$userTask1 = readline("Какая задача стоит перед вами сегодня?\n ");
+$deadline1 = readline("Сколько минут это займет?\n ");
+$userTask2 = readline("Что будешь делать дальше?\n ");
+$deadline2 = readline("Сколько минут это займет?\n ");
+$userTask3 = readline("А ещё?\n ");
+$deadline3 = readline("Сколько минут это займет?\n ");
 
-$userAge = date("Y") - $userYearBirth;
+$toDoList = "- $userTask1, это займет $deadline1 минут\n - $userTask2, это займет $deadline2 минут\n - $userTask3, это займет $deadline3 минут";
+$leadTime = floor(($deadline1 + $deadline2 + $deadline3)/60);
 
-echo "Приветствую тебя, $userAge летний пользователь $userName !!!\n";
+echo "$userName, сегодня у вас запланировано 3 приоритетных задачи на день: \n $toDoList \n";
+echo "Примерное время выполнения плана = {$leadTime}ч \n";
