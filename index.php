97012c18fbf4afb9a5a68ab4021b6d3962265db3
Rostislav <khmelinin.r@gmail.com>
2022-04-30 14:04:32 +0900

    basic php lesson 1 task 3 finish

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
 
 $userName = readline("Как зовут вас пользователь?\n ");
+$deadlineTemplate = "Сколько времени это займет?\n ";
 
 $userTask1 = readline("Какая задача стоит перед вами сегодня?\n ");
-$deadline1 = readline("Сколько минут это займет?\n ");
-$userTask2 = readline("Что будешь делать дальше?\n ");
-$deadline2 = readline("Сколько минут это займет?\n ");
-$userTask3 = readline("А ещё?\n ");
-$deadline3 = readline("Сколько минут это займет?\n ");
+$deadline1 = readline($deadlineTemplate);
 
-$toDoList = "- $userTask1, это займет $deadline1 минут\n - $userTask2, это займет $deadline2 минут\n - $userTask3, это займет $deadline3 минут";
-$leadTime = floor(($deadline1 + $deadline2 + $deadline3)/60);
+$userTask2 = readline("Что будешь делать дальше?\n ");
+$deadline2 = readline($deadlineTemplate);
+
+$userTask3 = readline("А ещё?\n ");
+$deadline3 = readline($deadlineTemplate);
+
+$toDoList = "
+- $userTask1, это займет $deadline1 минут\n
+- $userTask2, это займет $deadline2 минут\n
+- $userTask3, это займет $deadline3 минут\n";
+
+$leadTime = ($deadline1 + $deadline2 + $deadline3);
 
 echo "$userName, сегодня у вас запланировано 3 приоритетных задачи на день: \n $toDoList \n";
 echo "Примерное время выполнения плана = {$leadTime}ч \n";
